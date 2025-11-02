906d5810e7bf7e7365cec213d6fd1a0e5daea1fb
VishalSaini7272 <sainivishal156@gmail.com>
2025-11-02 17:11:10 +0530

    Implement Menu component to display items

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,1 +1,25 @@
+import React from "react";
+
+function Menu({ items }) {
+  return (
+    <div className="menu-container">
+      {items.map((item) => (
+        <div
+          key={item.id}
+          className="menu-item"
+          data-test-id={`menu-item-${item.category}`}
+        >
+          <img src={item.img} alt={item.name} className="menu-img" />
+          <div className="menu-info">
+            <h3>{item.name}</h3>
+            <p className="price">{item.price}</p>
+            <p className="category">{item.category}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Menu;
 
